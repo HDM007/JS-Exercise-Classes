3e8ac8a2c9140d771f327a209998d2ed98c5fe1d
hdm007 <harry.mardirossian13@gmail.com>
2020-12-17 17:32:18 -0500

    Completed Person class

--- index.js
+++ index.js
@@ -42,7 +42,26 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age){
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    }
+    eat(edible){
+      if (this.stomach.length >= 10){
+        return;
+      } else {
+        return this.stomach.push(edible)
+      }
+    };
+    poop(){
+      this.stomach = [];
+    };
+    toString(){
+      return `${this.name}, ${this.age}`
+    };
+
+
   }
   
   /*
